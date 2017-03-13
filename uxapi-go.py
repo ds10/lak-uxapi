fa541fe0c803e3d14026dd8f4f2c976691d02829
Ubuntu <ubuntu@ip-172-31-23-112.eu-west-1.compute.internal>
2017-03-13 18:08:11 +0000

    some practical updates: -only process tsv files in the incoming directory; -catch errors parsing the json template and tsv and write to the output rather than stacktracing; -added a shebang line

--- uxapi-go.py
+++ uxapi-go.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from configreader import configreader
 from xapiconverter import xapiconverter
 import dateutil.parser
@@ -16,18 +17,26 @@ for row in masterconfig:
         conv = xapiconverter()
         for filename in os.listdir(c.uxapilocation):
             arr = filename.split('.')
-            try:
-                d = open('timestamps/' + inst + '-' + arr[0] + '.txt', 'r')
-                ts = d.read()
-                ts = dateutil.parser.parse(ts)
-                d.close()
-            except:
-               ts = dateutil.parser.parse("2000-01-01T01:01:01Z")
+            if arr[1]=='tsv':
+                try:
+                    d = open('timestamps/' + inst + '-' + arr[0] + '.txt', 'r')
+                    ts = d.read()
+                    ts = dateutil.parser.parse(ts)
+                    d.close()
+                except:
+                    ts = dateutil.parser.parse("2000-01-01T01:01:01Z")
 
-            print arr[0] + ' data found'
-            latest = conv.readandconvert(ts,arr[0],c.uxapilocation+filename,c.homepage,c.xapiusername,c.xapipassword)
-            d = open('timestamps/' + inst + '-' + arr[0] + '.txt', 'w')
-            d.write(latest)
-            d.close()
+                print '\'' + arr[0] + '\' data found'
+
+                try:
+                    latest = conv.readandconvert(ts,arr[0],c.uxapilocation+filename,c.homepage,c.xapiusername,c.xapipassword)
+                    d = open('timestamps/' + inst + '-' + arr[0] + '.txt', 'w')
+                    d.write(latest)
+                    d.close()
+                except Exception as e:
+                    print "failure processing " + filename + " with " + arr[0] + ".json: " + str(e)
+
+
+
 
 
